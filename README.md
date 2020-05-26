8d43f0804ce37e75cbf2070d94920a5ee6412d03
Ross Wu <rosswu@rosswu-macbookpro2.roam.corp.google.com>
2020-05-27 00:04:52 +0800

    finish the algorithm

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # cube
 Rubik's cube resolver.
 
+## Usage
+
+### Step 1. Build the `kociemba` cube solver tool.
+
 `kociemba/` contains C99 implementation of Kociemba's two-phase algorithm
 for solving Rubik's Cube.
 It's forked from `https://github.com/muodov/kociemba`, please refer to the
@@ -13,3 +17,30 @@ $ cd kociemba/
 $ make
 $ ./bin/kociemba DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD
 ```
+
+### Step 2. Build the LEGO ev3 solver
+
+```
+$ go build main.go
+```
+
+#### Usage:
+
+**Simple usage**
+
+```
+$ cat in.1 | ./main
+```
+
+**Step-by-step output**
+
+```
+$ cat demo.in | ./main -v
+```
+
+**Step-by-step w/ debug info**
+
+```
+$ cat demo.in |./main --debug
+```
+
--- main.go
+++ main.go
@@ -17,6 +17,8 @@ var (
 	port      = flag.Int("port", 8080, "http server port")
 	kociemba  = flag.String("kociemba", "./kociemba/bin/kociemba", "Path to the Kociemba's Rubik's Cube solver binary.")
 	initMoves = flag.String("init_move", "", "Comma-separated initial moves, for test only.")
+	verbose   = flag.Bool("v", false, "Print the cube for each step.")
+	debug     = flag.Bool("debug", false, "debug mode.")
 )
 
 type Color int
@@ -31,23 +33,23 @@ const (
 	Orange
 	Unknown
 
-	Top    = "top"
-	Left   = "left"
-	Front  = "front"
-	Right  = "right"
-	Back   = "back"
-	Bottom = "bottom"
+	Up    byte = 'U'
+	Left  byte = 'L'
+	Front byte = 'F'
+	Right byte = 'R'
+	Back  byte = 'B'
+	Down  byte = 'D'
 )
 
 var (
-	faceNames = []string{Top, Left, Front, Right, Back, Bottom}
-	faceCode  = map[string]byte{
-		Top:    'U',
-		Left:   'L',
-		Front:  'F',
-		Right:  'R',
-		Back:   'B',
-		Bottom: 'D',
+	faceCodes = []byte{Up, Left, Front, Right, Back, Down}
+	faceNames = map[byte]string{
+		Up:    "up",
+		Left:  "left",
+		Front: "front",
+		Right: "right",
+		Back:  "back",
+		Down:  "down",
 	}
 	colors = map[Color]string{
 		White:   "\033[37m",
@@ -58,52 +60,30 @@ var (
 		Orange:  "\033[1;31m",
 		Unknown: "\033[30m",
 	}
-	leftNeighbor = map[string]string{
-		Left:  Back,
+	turnChain = map[byte]byte{
 		Front: Left,
-		Right: Front,
+		Left:  Back,
 		Back:  Right,
+		Right: Front,
 	}
-	rightNeighbor = map[string]string{
-		Left:  Front,
-		Front: Right,
-		Right: Back,
-		Back:  Left,
-	}
-	topNeighbor    = map[string]string{}
-	bottomNeighbor = map[string]string{}
 )
 
 type Face struct {
 	Pieces [9]Color
 }
 
+func FaceName(code byte) string {
+	return faceNames[code]
+}
+
 type Cube struct {
-	faces map[string]*Face
-	moves map[string]func(*Cube) int
+	faces map[byte]*Face
+	moves map[Move]func(*Cube) int
 
 	// When flip or turn cube, the real faces change positions, but to apply the
-	// solving algoritm, I'll the virual cube steady, so use var calibs to keep
+	// solving algoritm, I'll let the virual cube steady, so use var calibs to keep
 	// the mapping between virtual face to the real face.
-	calibs map[string]string
-}
-
-func (c *Cube) kociembaScramble() string {
-	colorToCode := map[Color]byte{}
-	for name, face := range c.faces {
-		colorToCode[face.Pieces[4]] = faceCode[name]
-	}
-	buf := make([]byte, 0, 55)
-	for _, name := range []string{Top, Right, Front, Bottom, Left, Back} {
-		for _, color := range c.faces[name].Pieces {
-			buf = append(buf, colorToCode[color])
-		}
-	}
-	return string(buf)
-}
-
-func (c *Cube) Face(name string) *Face {
-	return c.faces[c.calibs[name]]
+	calibs map[byte]byte
 }
 
 func rotateClock(f *Face) {
@@ -138,28 +118,46 @@ func rotateCounterclock(f *Face) {
 	f.Pieces[3] = saved
 }
 
+func invert(f *Face) {
+	f.Pieces[0], f.Pieces[8] = f.Pieces[8], f.Pieces[0]
+	f.Pieces[1], f.Pieces[7] = f.Pieces[7], f.Pieces[1]
+	f.Pieces[2], f.Pieces[6] = f.Pieces[6], f.Pieces[2]
+	f.Pieces[3], f.Pieces[5] = f.Pieces[5], f.Pieces[3]
+}
+
 // flips the cube bottom->front->top->back->bottom.
 func (c *Cube) flip() *Cube {
-	last := Top
-	saved := c.faces[Top]
-	for _, name := range []string{Front, Bottom, Bottom, Back} {
-		c.faces[last] = c.faces[name]
-		last = name
+	last := Up
+	saved := c.faces[Up]
+	for _, code := range []byte{Front, Down, Back} {
+		c.faces[last] = c.faces[code]
+		last = code
 	}
 	c.faces[Back] = saved
 
+	invert(c.faces[Back])
+	invert(c.faces[Down])
 	rotateClock(c.faces[Right])
 	rotateCounterclock(c.faces[Left])
 
-	c.calibs[Top] = Back
-	c.calibs[Back] = Bottom
-	c.calibs[Bottom] = Front
-	c.calibs[Front] = Top
+	// Update calibs.
+	for code := range c.calibs {
+		switch c.calibs[code] {
+		case Up:
+			c.calibs[code] = Back
+		case Front:
+			c.calibs[code] = Up
+		case Down:
+			c.calibs[code] = Front
+		case Back:
+			c.calibs[code] = Down
+		}
+	}
+	if *debug {
+		fmt.Printf("flip:\n")
+		c.Print()
+	}
 	return c
-}
-
-func (c *Cube) reverseFlip() *Cube {
-	return c.flip().flip().flip()
 }
 
 // turns the cube front->left->back->right->front.
@@ -167,19 +165,32 @@ func (c *Cube) turn(n int) *Cube {
 	for i := 0; i < n; i++ {
 		last := Left
 		saved := c.faces[Left]
-		for _, name := range []string{Left, Front, Right, Back} {
-			c.faces[last] = c.faces[name]
-			last = name
+		for _, code := range []byte{Left, Front, Right, Back} {
+			c.faces[last] = c.faces[code]
+			last = code
 		}
 		c.faces[Back] = saved
 
-		rotateClock(c.faces[Top])
-		rotateCounterclock(c.faces[Bottom])
+		rotateClock(c.faces[Up])
+		rotateCounterclock(c.faces[Down])
 
-		c.calibs[Front] = Left
-		c.calibs[Left] = Back
-		c.calibs[Back] = Right
-		c.calibs[Right] = Front
+		// Update calibs.
+		for code := range c.calibs {
+			switch c.calibs[code] {
+			case Left:
+				c.calibs[code] = Back
+			case Front:
+				c.calibs[code] = Left
+			case Right:
+				c.calibs[code] = Front
+			case Back:
+				c.calibs[code] = Right
+			}
+		}
+	}
+	if *debug {
+		fmt.Printf("turn=%d:\n", n)
+		c.Print()
 	}
 	return c
 }
@@ -193,7 +204,7 @@ func (c *Cube) D() *Cube {
 	last := Left
 	left := c.faces[Left].Pieces
 	saved := [3]Color{left[6], left[7], left[8]}
-	for _, cur := range []string{Front, Right, Back} {
+	for _, cur := range []byte{Back, Right, Front} {
 		for i := 6; i < 9; i++ {
 			c.faces[last].Pieces[i] = c.faces[cur].Pieces[i]
 		}
@@ -203,7 +214,7 @@ func (c *Cube) D() *Cube {
 		c.faces[last].Pieces[i+6] = saved[i]
 	}
 
-	rotateClock(c.faces[Bottom])
+	rotateClock(c.faces[Down])
 	return c
 }
 
@@ -215,9 +226,9 @@ func (c *Cube) d() *Cube {
 	return c.D().D().D()
 }
 
-func newMoves() map[string]func(*Cube) int {
+func newMoves() map[Move]func(*Cube) int {
 	// Basic moves.
-	m := map[string]func(*Cube) int{
+	m := map[Move]func(*Cube) int{
 		"D": func(c *Cube) int {
 			c.D()
 			return 1
@@ -302,22 +313,28 @@ func newMoves() map[string]func(*Cube) int {
 
 func NewCube() *Cube {
 	return &Cube{
-		faces: map[string]*Face{},
+		faces: map[byte]*Face{},
 		moves: newMoves(),
-		calibs: map[string]string{
-			Top:    Top,
-			Left:   Left,
-			Front:  Front,
-			Right:  Right,
-			Back:   Back,
-			Bottom: Bottom,
+		calibs: map[byte]byte{
+			Up:    Up,
+			Left:  Left,
+			Front: Front,
+			Right: Right,
+			Back:  Back,
+			Down:  Down,
 		},
 	}
 }
 
-// Rotate rotates cube fase according to the given move.
+func (c *Cube) Calib(old Move) Move {
+	b := []byte(old)
+	b[0] = c.calibs[b[0]]
+	return Move(b)
+}
+
+// Rotate rotates cube fase according to the given move. The move should be the virtual face.
 func (c *Cube) Rotate(m Move) error {
-	op, ok := c.moves[string(m)]
+	op, ok := c.moves[c.Calib(m)]
 	if !ok {
 		return fmt.Errorf("no such move: %s", m)
 	}
@@ -325,8 +342,53 @@ func (c *Cube) Rotate(m Move) error {
 	return nil
 }
 
-func (c *Cube) SetFace(name string, face *Face) {
-	c.faces[name] = face
+func (c *Cube) KociembaScramble() string {
+	colorToCode := map[Color]byte{}
+	for code, face := range c.faces {
+		colorToCode[face.Pieces[4]] = c.calibs[code]
+	}
+	buf := make([]byte, 0, 55)
+	for _, name := range []byte{Up, Right, Front, Down, Left, Back} {
+		for _, color := range c.faces[name].Pieces {
+			buf = append(buf, colorToCode[color])
+		}
+	}
+	return string(buf)
+}
+
+func (c *Cube) CalibsDebugString() string {
+	s := "{"
+	for _, from := range []byte{Up, Left, Front, Right, Back, Down} {
+		to := c.calibs[from]
+		s += fmt.Sprintf(" %c->%c", from, to)
+	}
+	s += " }"
+	return s
+}
+
+func (c *Cube) Apply(moves []string, printStep bool) error {
+	for i := range moves {
+		m := Move(strings.TrimSpace(moves[i]))
+		if len(m) == 0 {
+			continue
+		}
+		if *verbose {
+			fmt.Printf("calibs: %s\n", c.CalibsDebugString())
+		}
+		if err := c.Rotate(m); err != nil {
+			log.Printf("ERROR: Rotate(%s) error: %v", string(m), err)
+			return err
+		}
+		if printStep {
+			fmt.Printf("Step[%d]: newMove=%s\n", i+1, m)
+			c.Print()
+		}
+	}
+	return nil
+}
+
+func (c *Cube) SetFace(code byte, face *Face) {
+	c.faces[code] = face
 }
 
 func printColor(color Color) {
@@ -356,7 +418,7 @@ func (c *Cube) Print() {
 	// Print top face.
 	for row := 0; row < 3; row++ {
 		fmt.Printf("\t%s", indent)
-		printRow(c.faces[Top], row)
+		printRow(c.faces[Up], row)
 		fmt.Printf("│\n")
 	}
 
@@ -377,7 +439,7 @@ func (c *Cube) Print() {
 	// Print bottom face.
 	for row := 0; row < 3; row++ {
 		fmt.Printf("\t%s", indent)
-		printRow(c.faces[Bottom], row)
+		printRow(c.faces[Down], row)
 		fmt.Printf("│\n")
 	}
 
@@ -430,14 +492,14 @@ func readFace(faceName string, reader *bufio.Reader) *Face {
 }
 
 func solve(c *Cube) []string {
-	s := c.kociembaScramble()
+	s := c.KociembaScramble()
 	log.Printf("INFO: exec: %s %s", *kociemba, s)
 	out, err := exec.Command(*kociemba, s).Output()
 	if err != nil {
 		log.Printf("ERROR: failed to run %q: %v", *kociemba, err)
 		os.Exit(255)
 	}
-	return strings.Split(string(out), " ")
+	return strings.Split(strings.TrimSpace(string(out)), " ")
 }
 
 func main() {
@@ -447,25 +509,42 @@ func main() {
 	fmt.Println("Colors are: White Red Green Blue Yellow Orange, or w r g b y o.")
 	fmt.Println("(input 9 whitespace-separated colors for each face):")
 
+	if *debug {
+		*verbose = true
+	}
+
 	c := NewCube()
 	reader := bufio.NewReader(os.Stdin)
-	for _, faceName := range faceNames {
-		c.SetFace(faceName, readFace(faceName, reader))
+	for _, code := range faceCodes {
+		c.SetFace(code, readFace(FaceName(code), reader))
 	}
 
 	fmt.Printf("\n\nINPUT:\n")
 	c.Print()
 
 	if len(*initMoves) > 0 {
-		for _, m := range strings.Split(*initMoves, ",") {
-			c.Rotate(Move(m))
-		}
 		fmt.Println("------------------------------------------------------")
-		fmt.Printf("AFTER INITIAL MOVES:\n")
-		c.Print()
+		fmt.Printf("INITIAL MOVES:\n")
+		for _, m := range strings.Split(*initMoves, ",") {
+			fmt.Printf("\nmove=%s (->%s):\n", m, c.Calib(Move(m)))
+			fmt.Printf("calibs: %s\n", c.CalibsDebugString())
+			c.Rotate(Move(m))
+			c.Print()
+		}
+		os.Exit(0)
 	}
 
 	steps := solve(c)
 	fmt.Println("------------------------------------------------------")
-	fmt.Printf("SOLUTION: total steps: %d: %s\n", len(steps), strings.Join(steps, " "))
+	fmt.Printf("SOLUTION: step=%d: %s\n", len(steps), strings.Join(steps, " "))
+
+	if err := c.Apply(steps, *verbose); err != nil {
+		fmt.Printf("ERROR: Apply(%v) error: %v", steps, err)
+		os.Exit(255)
+	}
+	if !*verbose {
+		c.Print()
+	}
+
+	fmt.Printf("\nDONE\n")
 }
